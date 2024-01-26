183ac8f63e0efc4101cebeb2d8d9a4f4ce12e548
filippogiovannoni <filippogiovannonii@gmail.com>
2024-01-26 11:07:30 +0100

    Initial commit add Milestone 1

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      todolist: [
+        {
+          text: 'Portare fuori il cane',
+          done: false
+        },
+        {
+          text: 'Rispondere alle e-mail urgenti',
+          done: true
+        },
+        {
+          text: 'Terminare il progetto entro fine settimana',
+          done: false
+        },
+        {
+          text: 'Andare a correre per 30 minuti',
+          done: true
+        },
+        {
+          text: 'Fare la spesa',
+          done: true
+        },
+        {
+          text: 'Pulire la casa',
+          done: false
+        },
+        {
+          text: 'Fare rifornimento alla macchina',
+          done: false
+        },
+        {
+          text: 'Comprare il biglietto per lo stadio',
+          done: true
+        },
+      ],
+      crossed: 'crossed'
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>Todolist</h1>
+    <ul>
+      <li v-for="todo in todolist">
+        <span v-if="todo.done === true" :class="crossed">{{ todo.text }}</span>
+        <span v-else>{{ todo.text }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style></style>
